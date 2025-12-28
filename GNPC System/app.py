bf62299c8679c1d78cd19a684a439e5c1da7fcf2
Annan-404 <papaannan404@gmail.com>
2025-12-28 21:18:32 +0000

    app.py

--- GNPC System/app.py
+++ GNPC System/app.py
@@ -281,7 +281,11 @@ def logout():
 # =========================
 # RUN APP
 # =========================
+# if __name__ == "__main__":
+#     app.run(port=5001)
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(host="0.0.0.0", port=10000)
+
+
 
 
